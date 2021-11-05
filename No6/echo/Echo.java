c3c3f65bc9249278dde4e782b1d4bde4ea88a10b
tsyu-ubuntu-note <syukunt@icloud.com>
2021-11-05 12:27:09 +0900

    No6提出用

--- No6/echo/Echo.java
+++ No6/echo/Echo.java
@@ -16,11 +16,15 @@ public class Echo {
         n = scanner.nextInt();
     }
     
+
     
     private void echo() throws InterruptedException {
+        WaitUserInput wui = new WaitUserInput();
+        wui.start();
         while(n != 0) {
             System.out.print(n);
             Thread.sleep(SLEEP_LEN_MSEC);
+            n = wui.m != -1 ? wui.m : n;
         }
     }
     
@@ -30,4 +34,20 @@ public class Echo {
         ste.echo();
     }
 
+}
+
+
+class WaitUserInput extends Thread {
+    private Scanner scanner;
+    public int m = -1;
+
+    public void run() {
+        while(true) {
+            scanner = new Scanner(System.in);
+            m = scanner.nextInt();
+            if(m == 0) {
+                break;
+            }
+        }        
+    }
 }

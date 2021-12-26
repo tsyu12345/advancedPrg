e7bcfbabd13de46011bc88bfe405c3275d6e4758
syu-ubuntu-homePC <syukunt@gmail.com>
2021-12-26 21:53:35 +0900

    seleniumの設置と配置を完了。

---
+++ No10/report2/application/Scraping/JudgeNumber/src/App.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.setProperty("webdriver.chrome.driver", "lib/chromedriver_linux64/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.google.com/xhtml");
+        Thread.sleep(5000);  // Let the user actually see something!
+        WebElement searchBox = driver.findElement(By.name("q"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        Thread.sleep(5000);  // Let the user actually see something!
+        driver.quit();
+    }
+}
